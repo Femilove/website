6f567c96e848d7ccc6fd44ce97dc7c5f3408cb1b
Femilove <144688729+Femilove@users.noreply.github.com>
2024-10-30 19:44:10 +0100

    views.py

--- main/views.py
+++ main/views.py
@@ -16,7 +16,7 @@ def login(response):
 
 
 def sign(response):
-    return render(response, 'register.html')
+    return render(response, 'Register.html')
 
 
 def index(response):
@@ -64,10 +64,10 @@ def register(response):
         if password1 == password2:
             if User.objects.filter(username=username).exists():
                 messages.info(response, 'Username Taken')
-                return redirect('register.html')
+                return redirect('Register.html')
             elif User.objects.filter(email=email).exists():
                 messages.info(response, 'Email Taken')
-                return redirect('register.html')
+                return redirect('Register.html')
             elif superuser == 'A201' or superuser == 'A210' or superuser == 'A200' or superuser == 'A210' or superuser == 'A211' or superuser == 'AO21' or superuser == 'A012' or superuser == 'A120' or superuser == 'A001' or superuser == 'A011' or superuser == 'A000' or superuser == 'A100' or superuser == 'A010' or superuser == 'A110' or superuser == 'B221' or superuser == 'B002' or superuser == 'B111' or superuser == 'B001' or superuser == 'B220' or superuser == 'B200' or superuser == 'B000' or superuser == 'B110' or superuser == 'B201' or superuser == 'B112' or superuser == 'B121' or superuser == 'B211' or superuser == 'B222':
                 user = User.objects.create_user(
                     username=username, password=password1, email=email, first_name=first_name, last_name=last_name)
@@ -77,13 +77,13 @@ def register(response):
                 return redirect('login.html')
             else:
                 messages.info(response, 'superuser not correct')
-                return redirect('register.html')
+                return redirect('Register.html')
         else:
             messages.info(response, 'Password not matching')
-            return redirect('register.html')
+            return redirect('Register.html')
 
     else:
-        return render(response, 'register.html')
+        return render(response, 'Register.html')
 
 
 # creating a fuction for login.url
