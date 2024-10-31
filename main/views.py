17ca16f6b4a824787fb67796e1957fdc6e5d44cc
Femilove <144688729+Femilove@users.noreply.github.com>
2024-11-01 00:00:13 +0100

    views.py

--- main/views.py
+++ main/views.py
@@ -64,26 +64,26 @@ def register(response):
         if password1 == password2:
             if User.objects.filter(username=username).exists():
                 messages.info(response, 'Username Taken')
-                return redirect('Register.html')
+                return redirect('Register')
             elif User.objects.filter(email=email).exists():
                 messages.info(response, 'Email Taken')
-                return redirect('Register.html')
+                return redirect('Register')
             elif superuser == 'A201' or superuser == 'A210' or superuser == 'A200' or superuser == 'A210' or superuser == 'A211' or superuser == 'AO21' or superuser == 'A012' or superuser == 'A120' or superuser == 'A001' or superuser == 'A011' or superuser == 'A000' or superuser == 'A100' or superuser == 'A010' or superuser == 'A110' or superuser == 'B221' or superuser == 'B002' or superuser == 'B111' or superuser == 'B001' or superuser == 'B220' or superuser == 'B200' or superuser == 'B000' or superuser == 'B110' or superuser == 'B201' or superuser == 'B112' or superuser == 'B121' or superuser == 'B211' or superuser == 'B222':
                 user = User.objects.create_user(
                     username=username, password=password1, email=email, first_name=first_name, last_name=last_name)
                 user.save()
                 print('user Created')
 
-                return redirect('login.html')
+                return redirect('login')
             else:
                 messages.info(response, 'superuser not correct')
-                return redirect('Register.html')
+                return redirect('Register')
         else:
             messages.info(response, 'Password not matching')
-            return redirect('Register.html')
+            return redirect('Register')
 
     else:
-        return render(response, 'Register.html')
+        return render(response, 'Register')
 
 
 # creating a fuction for login.url
@@ -102,10 +102,10 @@ def login(response):
         else:
 
             messages.info(response, 'Invaliid credencials')
-            return redirect("login.html")
+            return redirect("login")
 
     else:
-        return render(response, 'login.html')
+        return render(response, 'login')
 
 # creating a fuction for logout.url
 
